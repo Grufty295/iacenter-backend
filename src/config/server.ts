1c9a12292118f1d78368bd9ebaf6f3a13e741d2c
Grufty295 <sebastian.audetat.f@gmail.com>
2021-10-06 12:24:13 -0600

    fix:  error handler and logger middlewares routes

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,7 +1,7 @@
 import express, { Application } from 'express'
 import cors from 'cors'
-import errorMiddleware from 'middleware/error.middleware'
-import loggerMiddleware from 'middleware/logger.middleware'
+import errorMiddleware from '../middleware/error.middleware'
+import loggerMiddleware from '../middleware/logger.middleware'
 
 class Server {
   private app: Application
